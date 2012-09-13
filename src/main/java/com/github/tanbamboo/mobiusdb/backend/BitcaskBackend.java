3ac9972c0b7bf118717db4db58fdda3fcb0a49a9
Benjamin Tan <tanbamboo@gmail.com>
2012-09-13 11:29:05 +0800

    initial commit project pom and code

---
+++ src/main/java/com/github/tanbamboo/mobiusdb/backend/BitcaskBackend.java
@@ -0,0 +1,14 @@
+/**
+ * %%
+ * Copyright (C) 2012 Benjamin Tan
+ * %%
+ */
+package com.github.tanbamboo.mobiusdb.backend;
+
+/**
+ * @author ben
+ *
+ */
+public class BitcaskBackend implements KeyValueBackend {
+
+}
